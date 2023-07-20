4352bd750354f2e03cce09455593e450c843512c
LorenzoBondan <lbbondan@gmail.com>
2023-07-20 19:19:41 -0300

    added more data

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -13,16 +13,38 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_book (title, author, year, img_Url) VALUES ('Harry Potter e a Câmara Secreta', 'J.K. Rowling', 2001, 'https://livrariakadernus.com.br/wp-content/uploads/2021/09/81jbivNEVML1.jpg');
 INSERT INTO tb_book (title, author, year, img_Url) VALUES ('Percy Jackson e o Ladrão de raios', 'Rick Riordan', 2002, 'https://m.media-amazon.com/images/I/61JenSx3wKL._AC_UF1000,1000_QL80_.jpg');
 INSERT INTO tb_book (title, author, year, img_Url) VALUES ('A sociedade do anel', 'J.R.R Tolkien', 2003, 'https://http2.mlstatic.com/D_NQ_NP_956499-MLU50423691640_062022-O.webp');
+INSERT INTO tb_book (title, author, year, img_Url) VALUES ('Dom Quixote', 'Miguel de Cervantes', 1605, 'https://www.lpm.com.br/livros/imagens/dom_quixote_hq_9788525433633_hd.jpg');
+INSERT INTO tb_book (title, author, year, img_Url) VALUES ('Orgulho e Preconceito', 'Jane Austen', 1813, 'https://m.media-amazon.com/images/I/61JosO2tkdL._AC_UF1000,1000_QL80_.jpg');
+INSERT INTO tb_book (title, author, year, img_Url) VALUES ('1984', 'George Orwell', 1949, 'https://m.media-amazon.com/images/I/5126eSrThKL._AC_UF1000,1000_QL80_.jpg');
+INSERT INTO tb_book (title, author, year, img_Url) VALUES ('Cem Anos de Solidão', 'Gabriel García Márquez', 1967, 'https://m.media-amazon.com/images/I/51cfxI-51mL.jpg');
+INSERT INTO tb_book (title, author, year, img_Url) VALUES ('Crime e Castigo', 'Fiódor Dostoiévski', 1866, 'https://m.media-amazon.com/images/I/41SXsI+ahpL.jpg');
+INSERT INTO tb_book (title, author, year, img_Url) VALUES ('O Pequeno Príncipe', 'Antoine de Saint-Exupéry', 1943, 'https://www.gruponovoseculo.com.br/image/cache/catalog/9/7/9788542805956-480x672.jpg');
 
 INSERT INTO tb_user_mybooks (user_id, book_id) VALUES (1, 1);
 INSERT INTO tb_user_mybooks (user_id, book_id) VALUES (2, 2);
 INSERT INTO tb_user_mybooks (user_id, book_id) VALUES (3, 3);
+INSERT INTO tb_user_mybooks (user_id, book_id) VALUES (1, 4);
+INSERT INTO tb_user_mybooks (user_id, book_id) VALUES (2, 5);
+INSERT INTO tb_user_mybooks (user_id, book_id) VALUES (3, 6);
+INSERT INTO tb_user_mybooks (user_id, book_id) VALUES (1, 7);
+INSERT INTO tb_user_mybooks (user_id, book_id) VALUES (2, 8);
+INSERT INTO tb_user_mybooks (user_id, book_id) VALUES (2, 9);
 
 INSERT INTO tb_user_wishlist (user_id, book_id) VALUES (1, 2);
 INSERT INTO tb_user_wishlist (user_id, book_id) VALUES (2, 3);
 INSERT INTO tb_user_wishlist (user_id, book_id) VALUES (3, 1);
+INSERT INTO tb_user_wishlist (user_id, book_id) VALUES (2, 4);
+INSERT INTO tb_user_wishlist (user_id, book_id) VALUES (2, 6);
+INSERT INTO tb_user_wishlist (user_id, book_id) VALUES (2, 7);
 
 INSERT INTO tb_exchange (status, creation_Time, creator_id, receiver_id, book_Offered_id, book_Received_id) VALUES (0, TIMESTAMP WITHOUT TIME ZONE '2023-06-12T22:30:12', 2, null, 2, null);
 INSERT INTO tb_exchange (status, creation_Time, creator_id, receiver_id, book_Offered_id, book_Received_id) VALUES (1, TIMESTAMP WITHOUT TIME ZONE '2023-06-12T22:30:12', 2, 1, 2, 1);
+INSERT INTO tb_exchange (status, creation_Time, creator_id, receiver_id, book_Offered_id, book_Received_id) VALUES (0, TIMESTAMP WITHOUT TIME ZONE '2023-07-15T10:00:00', 1, null, 2, null);
+INSERT INTO tb_exchange (status, creation_Time, creator_id, receiver_id, book_Offered_id, book_Received_id) VALUES (1, TIMESTAMP WITHOUT TIME ZONE '2023-07-15T10:30:00', 3, 2, 1, 3);
+INSERT INTO tb_exchange (status, creation_Time, creator_id, receiver_id, book_Offered_id, book_Received_id) VALUES (2, TIMESTAMP WITHOUT TIME ZONE '2023-07-15T11:00:00', 2, 1, 3, 2);
+INSERT INTO tb_exchange (status, creation_Time, creator_id, receiver_id, book_Offered_id, book_Received_id) VALUES (0, TIMESTAMP WITHOUT TIME ZONE '2023-07-16T14:00:00', 3, null, 3, null);
+INSERT INTO tb_exchange (status, creation_Time, creator_id, receiver_id, book_Offered_id, book_Received_id) VALUES (1, TIMESTAMP WITHOUT TIME ZONE '2023-07-16T14:30:00', 2, 3, 1, 2);
+INSERT INTO tb_exchange (status, creation_Time, creator_id, receiver_id, book_Offered_id, book_Received_id) VALUES (2, TIMESTAMP WITHOUT TIME ZONE '2023-07-16T15:00:00', 3, 1, 3, 1);
 
 INSERT INTO tb_notification (description, read, moment, user_id) VALUES ('First notification', false, TIMESTAMP WITHOUT TIME ZONE '2023-06-12T22:30:12', 2);
+
